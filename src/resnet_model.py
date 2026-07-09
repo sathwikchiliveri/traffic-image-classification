390d0a8279044dfea3b67b553c85a2d087bd08cc
sathwikchiliveri <chiliverisathwik@gmail.com>
2026-07-10 00:49:25 +0530

    ADD Resnet model

---
+++ src/resnet_model.py
@@ -0,0 +1,70 @@
+"""
+resnet_model.py
+
+Pretrained ResNet18 for
+Traffic Image Classification
+"""
+
+import torch
+import torch.nn as nn
+from torchvision import models
+
+
+class TrafficResNet(nn.Module):
+
+    def __init__(self, num_classes=5):
+
+        super(TrafficResNet, self).__init__()
+
+        # ==========================================
+        # Load Pretrained ResNet18
+        # ==========================================
+
+        self.model = models.resnet18(
+            weights=models.ResNet18_Weights.DEFAULT
+        )
+
+        # ==========================================
+        # Freeze Feature Extractor
+        # ==========================================
+
+        for param in self.model.parameters():
+            param.requires_grad = False
+
+        # ==========================================
+        # Replace Final Layer
+        # ==========================================
+
+        in_features = self.model.fc.in_features
+
+        self.model.fc = nn.Sequential(
+
+            nn.Linear(in_features, 256),
+
+            nn.ReLU(),
+
+            nn.Dropout(0.5),
+
+            nn.Linear(256, num_classes)
+        )
+
+    def forward(self, x):
+
+        return self.model(x)
+
+
+# ==========================================
+# Test
+# ==========================================
+
+if __name__ == "__main__":
+
+    model = TrafficResNet()
+
+    dummy = torch.randn(1, 3, 224, 224)
+
+    output = model(dummy)
+
+    print(model)
+
+    print("\nOutput Shape:", output.shape)
